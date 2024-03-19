32ecbae83993b0b7356e071acc56040a3417c44e
robinsonnie <robinsonnie@github.com>
2024-03-19 19:23:59 +0800

    fix model_download 86400 timeout

--- src/model_download/index.py
+++ src/model_download/index.py
@@ -1,12 +1,26 @@
 import os 
+from datetime import datetime
+from threading import Timer
 from modelscope.hub.api import HubApi
 from modelscope.hub.snapshot_download import snapshot_download
+
+finish_flag = False
+def loop_log():
+    now = datetime.now()
+    ts = now.strftime("%Y-%m-%d %H:%M:%S")
+    print(ts)
+    if finish_flag == True:
+        return
+    t = Timer(10, loop_log)
+    t.start()
 
 def handler(event, context):
     model_id = os.getenv('MODEL_ID', '')
     revision = os.getenv('MODEL_VERSION', '')
     cache_dir = os.getenv('MODELSCOPE_CACHE', '')
     sdk_token = os.getenv('MODELSCOPE_TOKEN', '')
+    # avoid no response bytes in long periods
+    loop_log()
     # login first.
     HubApi().login(sdk_token)
     if len(revision) > 0:
@@ -17,3 +31,4 @@ def handler(event, context):
          snapshot_download (model_id =model_id, 
                             cache_dir = cache_dir)
     print("download model scuccess!")
+    finish_flag = True
