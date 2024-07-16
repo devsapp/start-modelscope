d9b518e66760b45c448ac98b9d01dec8570b8f26
suluyana <suluyan_sly@163.com>
2024-07-16 10:51:00 +0800

    deploy

--- src/model_deploy/index.py
+++ src/model_deploy/index.py
@@ -13,4 +13,22 @@ def handler(event, context):
     family = os.getenv('MODEL_FAMILY', '')
     served_model_name = os.getenv('SERVED_MODEL_NAME', '')
 
-    return {'served_model_name': served_model_name}
+    cmd = f'cd {cache_dir}/ollama-linux && sudo chmod 777 ./ollama-modelscope-install.sh && ./ollama-modelscope-install.sh'
+    os.system(cmd)
+    cmd = 'ollama serve &'
+    os.system(cmd)
+
+    if modelfile and len(modelfile):
+        os.system(f'cat {modelfile} > {cache_dir}/ModelFile')
+    elif family and len(family):
+        os.system(f'wget https://modelscope.oss-cn-beijing.aliyuncs.com/llm_template/ollama/{family}.modelfile')
+        command_gen_modelfile = f'cat {family}.modelfile | sed "s/' + '{gguf_file}' + f'/{gguf_file}/" > ./ModelFile'
+        os.system(command_gen_modelfile)
+    else:
+        raise ValueError(f'modelfile 和 model_family至少需要配置一个。用于ollama模型初始化。')
+    
+    # run ollama
+    cmd = f'ollama create {served_model_name} --file ./ModelFile'
+    os.system(cmd)
+    cmd = f'ollama run {served_model_name}'
+    os.system(cmd)
--- src/model_download/index.py
+++ src/model_download/index.py
@@ -9,8 +9,6 @@ def handler(event, context):
     sdk_token = os.getenv('MODELSCOPE_TOKEN', '')
     image_tag = os.getenv('IMAGE_TAG', '')
     gguf_file = os.getenv('GGUF_FILE', '')
-    modelfile = os.getenv('MODELFILE', '')
-    family = os.getenv('MODEL_FAMILY', '')
 
     # login first.
     HubApi().login(sdk_token)
@@ -29,14 +27,5 @@ def handler(event, context):
 
         command_download_model = f'modelscope download --model={model_id} --local_dir {cache_dir} ${gguf_file}'
         os.system(command_download_model)
-
-        if modelfile and len(modelfile):
-            os.system(f'cat {modelfile} > {cache_dir}/ModelFile')
-        elif family and len(family):
-            os.system(f'wget https://modelscope.oss-cn-beijing.aliyuncs.com/llm_template/ollama/{family}.modelfile')
-            command_gen_modelfile = f'cat {family}.modelfile | sed "s/' + '{gguf_file}' + f'/{gguf_file}/" > {cache_dir}/ModelFile'
-            os.system(command_gen_modelfile)
-        else:
-            raise ValueError(f'modelfile 和 model_family至少需要配置一个。用于ollama模型初始化。')
         
         print("download model scuccess!")
