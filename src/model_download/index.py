cb0254a529535dffbf1ec068f913ea8863dd9dd9
suluyana <suluyan_sly@163.com>
2024-08-01 15:22:15 +0800

    rename args

--- src/model_download/index.py
+++ src/model_download/index.py
@@ -8,9 +8,8 @@ def handler(event, context):
     cache_dir = os.getenv('MODELSCOPE_CACHE', '')
     sdk_token = os.getenv('MODELSCOPE_TOKEN', '')
     image_tag = os.getenv('IMAGE_TAG', '')
-    gguf_file = os.getenv('GGUF_FILE', '')
-    modelfile = os.getenv('MODELFILE', '')
-    name = os.getenv('SERVED_MODEL_NAME', '')
+    sub_model_file = os.getenv('SUB_MODEL_FILE', '')
+    template_file_url = os.getenv('TEMPLATE_FILE_URL', '')
 
     # login first.
     HubApi().login(sdk_token)
@@ -30,21 +29,22 @@ def handler(event, context):
         command_download_ollama = f'modelscope download --model=modelscope/ollama-linux --local_dir {cache_dir}/ollama-linux'
         os.system(command_download_ollama)
 
-        command_download_model = f'modelscope download --model={model_id} --local_dir {cache_dir} {gguf_file}'
+        command_download_model = f'modelscope download --model={model_id} --local_dir {cache_dir} {sub_model_file}'
         os.system(command_download_model)
 
         os.system(f'cd {cache_dir}/ollama-linux && chmod 777 ./ollama-modelscope-install.sh && ./ollama-modelscope-install.sh')
         os.system(f'OLLAMA_MODELS={cache_dir} OLLAMA_HOST=127.0.0.1:9000 ollama serve &')
-        os.system(f'wget {modelfile} -O /home/modelfile')
+        os.system(f'wget {template_file_url} -O /home/modelfile')
 
-        os.system(f'echo  {cache_dir}/{gguf_file}')
-        os.system(f'ln -snf {cache_dir}/{gguf_file} /home/{gguf_file}')
+        os.system(f'echo  {cache_dir}/{sub_model_file}')
+        os.system(f'ln -snf {cache_dir}/{sub_model_file} /home/{sub_model_file}')
 
         with open('/home/modelfile', 'r') as f_in, open('/home/ModelFile', 'w') as f_out:
             lines = f_in.readlines()
-            lines[0] = lines[0].replace('{gguf_file}', f'{gguf_file}')
+            assert lines[0].strip() == 'FROM {gguf_file}', '请不要修改配置文件第一行 `FROM {gguf_file}`。'
+            lines[0] = lines[0].replace('{gguf_file}', f'{sub_model_file}')
             f_out.writelines(lines)
 
-        os.system(f'OLLAMA_HOST=0.0.0.0:9000 ollama create {name} --file /home/ModelFile')
+        os.system(f'OLLAMA_HOST=0.0.0.0:9000 ollama create {model_id} --file /home/ModelFile')
 
         print("download model scuccess!")
--- src/model_meta_info/index.py
+++
@@ -1,8 +0,0 @@
-import os 
-from modelscope.hub.api import HubApi
-from modelscope.hub.snapshot_download import snapshot_download
-
-def handler(event, context):
-    served_model_name = os.getenv('SERVED_MODEL_NAME', '')
-
-    return {'served_model_name': served_model_name}
